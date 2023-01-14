8dc964679a2dda7379e1faf3a5513ff5c9fd1d1e
alicecha <110170331+alicecha@users.noreply.github.com>
2023-01-14 13:04:20 +0200

    Create function that plays a single round of the game
    The function takes two parameters: player choice and computer choice,
    and returns outcome of the round.
    Define the two parameters and log outcome in the console to test that
    the function works.

--- game.js
+++ game.js
@@ -4,17 +4,35 @@
 //Get computer to randomly pick "rock", "paper" or "scissors"
 //Create function that randomly picks string within array of 3
 function getComputerChoice() {
-//Declare an array of 3 strings
+    //Declare an array of 3 strings
     let choiceArray = ["Rock", "Paper", "Scissors"];
-//Randomly pick string from array, between position 1 and array length
+    //Randomly pick string from array, between position 1 and array length
     let computerChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)];
     return computerChoice;
 }
 
+//Create function that plays a single round of the game.
+//The function will take two parameters: the player selection and the computer selection.
+function playRound(playerSelection, computerSelection) {
+    //create outcome variable which states whether player wins or loses depending on both selections.
+    //log outcome in console
+    if (playerSelection == computerSelection) {
+        return outcome = `No one wins! You both picked ${playerSelection}.`;
+    } else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
+               (playerSelection == "Paper" && computerSelection == "Scissors") ||
+               (playerSelection == "Scissors" && computerSelection == "Rock")) {
+        return outcome = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return outcome = `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+}
 
+//Define the two parameters of the function and test result in the console:
+    //The player selection is fixed for now.
+    const playerSelection = "Rock";
+    //The computer selection is generated randomly from the getComputerChoice function
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
 
-
-//Get user to pick "rock", "paper" or "scissors"
-//Say which of computer or user won.
 
 
